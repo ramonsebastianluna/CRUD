2d7cacb5375c84d885b74e9f8d84724bc5a2c9e6
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-02-11 17:56:34 -0300

    responsive added to login

--- php/validacion.php
+++ php/validacion.php
@@ -14,7 +14,7 @@ $user = $miArray['usuario'];
 $pass = $miArray['contrasenia'];
 
 if ($usuario == $user && $contrasenia == $pass){
-    header("Location: home.php");
+    header("Location: ../home.html");
 } else {
     echo "<html><head><script>alert('Usuario o contraseña incorrecto');</script></head></html>";
     echo "<meta http-equiv='refresh' content='0; url=login.html'>";
